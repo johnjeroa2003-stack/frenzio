d5d6cbcb52f96a45aa172591cf6e1038ec9abd8b
Johnson Jerald <johnjeroa2003@gmail.com>
2026-04-23 03:11:19 +0530

    added root route

--- server.js
+++ server.js
@@ -84,4 +84,15 @@ function getUsers(room) {
     }));
 }
 
+app.get("/", (req, res) => {
+  res.send("Frenzio Backend Running 🚀");
+});
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Frenzio Backend Running 🚀");
+});
+
 server.listen(3000, () => console.log("🚀 Running on http://localhost:3000"));
